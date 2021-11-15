bd6ec468db4831adebdb9a07a871025f991d9db1
lijie <827893370@qq.com>
2021-11-15 19:56:20 +0800

    demo update

---
+++ vue-demo/acro-demo/src/components/HelloWorld.vue
@@ -0,0 +1,92 @@
+<script lang="ts">
+import { defineComponent } from "vue"
+
+const component = defineComponent({
+    data() {
+      return {
+        columns: [
+          {
+            title: 'Name',
+            dataIndex: 'name',
+          },
+          {
+            title: 'Salary',
+            dataIndex: 'salary',
+          },
+          {
+            title: 'Address',
+            dataIndex: 'address',
+          },
+          {
+            title: 'Email',
+            dataIndex: 'email',
+          },
+        ],
+        data: [{
+          key: '1',
+          name: 'Jane Doe',
+          salary: 23000,
+          address: '32 Park Road, London',
+          email: 'jane.doe@example.com'
+        }, {
+          key: '2',
+          name: 'Alisa Ross',
+          salary: 25000,
+          address: '35 Park Road, London',
+          email: 'alisa.ross@example.com'
+        }, {
+          key: '3',
+          name: 'Kevin Sandra',
+          salary: 22000,
+          address: '31 Park Road, London',
+          email: 'kevin.sandra@example.com'
+        }, {
+          key: '4',
+          name: 'Ed Hellen',
+          salary: 17000,
+          address: '42 Park Road, London',
+          email: 'ed.hellen@example.com'
+        }, {
+          key: '5',
+          name: 'William Smith',
+          salary: 27000,
+          address: '62 Park Road, London',
+          email: 'william.smith@example.com'
+        }]
+      }
+    }
+  })
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+
+  <a-table :columns="columns" :data="data">
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
